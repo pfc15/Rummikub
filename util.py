e924f9d991e5467658e93ac53d2cb5dad2ca7d6c
Pedro Cruz <pedrofcrux@gmail.com>
2025-05-26 15:59:34 -0300

    adicionando bubble sort

--- util.py
+++ util.py
@@ -23,4 +23,17 @@ def get_n_final(pecas, trinca=False):
     n = int(pecas[len(pecas)-cont][1])+cont
     return n if not trinca else int(pecas[cont][1])
 
+def bubble_sort(geracoes):
+    
+    n = len(geracoes)
+    for i in range(n):
+        swapped = False
+        for j in range(0, n - i - 1):
+            if geracoes[j].get_fitness() > geracoes[j+1].get_fitness():
+                geracoes[j], geracoes[j + 1] = geracoes[j + 1], geracoes[j]
+                swapped = True
+
+        if not swapped:
+            break
+    return geracoes
 
