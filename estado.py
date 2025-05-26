f81b8029d015650bb43555f9ecc7b064089c9885
Pedro Cruz <pedrofcrux@gmail.com>
2025-05-26 11:16:55 -0300

    deixou o estado de jogo em classe, mais organizado para ter multiplos estados de jogo

---
+++ estado.py
@@ -0,0 +1,45 @@
+from numpy.random import randint
+
+class Estado():
+    def __init__(self, mesa,jogador, pecas, vez=0,movimento=0, comeco=False):
+        self.mesa = mesa
+        self.jogador = jogador
+        self.pecas = pecas
+        self.vez = vez
+        self.movimento = movimento
+        if comeco:
+            self.comeca_jogo()
+        
+    def comeca_jogo(self):
+        self.pecas = self.cria_pecas()
+        self.jogador = []
+        for i in range(14):
+            index = randint(0, len(self.pecas))
+            self.jogador.append(self.pecas[index])
+            self.jogador.sort()
+            self.pecas.pop(index)
+    
+    def cria_pecas(self):
+        cores = ['c','o', 'p', 'e']
+        pecas = []
+        for e in range(2):
+            for cor in cores:
+                for i in range(1, 14):
+                    pecas.append(f'{cor}{i}')
+        # pecas.append('j0')
+        # pecas.append('j0')
+        return pecas
+
+    def get_fitness(self):
+        return (self.vez, len(self.jogador))
+    
+    def mesa_to_string(self):
+        s = ""
+        for j in self.mesa:
+            s += str(j)+"/"
+        return s
+
+    def to_string(self):
+        a = f"mesa:{self.mesa_to_string()};\n jogador: {self.jogador};\n vez: {self.vez};\npecas:{len(self.pecas)}"
+        return a
+
